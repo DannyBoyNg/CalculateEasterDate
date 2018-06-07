d0ce56835baf8f390131ee54d399169a03c611d4
Danny <dannyboyng@gmail.com>
2018-06-07 17:15:49 +0200

    Add project files.

---
+++ CalculateEasterDateForYear/EasterDate.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace CalculateEasterDateForYear
+{
+    public class EasterDate
+    {
+        public static DateTime GetDateForYear(int? yearArg = null)
+        {
+            int year;
+            year = (yearArg == null) ? DateTime.Now.Year : year = yearArg.Value;
+            int day = 0;
+            int month = 0;
+
+            int g = year % 19;
+            int c = year / 100;
+            int h = (c - c / 4 - (8 * c + 13) / 25 + 19 * g + 15) % 30;
+            int i = h - h / 28 * (1 - h / 28 * (29 / (h + 1)) * ((21 - g) / 11));
+
+            day = i - ((year + year / 4 + i + 2 - c + c / 4) % 7) + 28;
+            month = 3;
+            if (day > 31)
+            {
+                month++;
+                day -= 31;
+            }
+            return new DateTime(year, month, day);
+        }
+    }
+}
